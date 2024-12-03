b4483cbd9b247d69b6bec7f55641944845283874
unknown <pedroornelaspego@gmail.com>
2024-12-03 13:50:16 -0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,108 @@
 import "./App.scss";
-
-import Button from "react-bootstrap/Button";
+import React from "react";
+import Container from "react-bootstrap/Container";
 import Menubar from "./components/Menubar";
+import { Card, Button } from "react-bootstrap";
+import logo from "./assets/background/logo.jpg";
 
 function App() {
   return (
-    <div>
-      <Menubar></Menubar>
-      <h1>TESTE</h1>
-      <Button variant="primary">Primary</Button>
+    <div className="app">
+      <Menubar />
+
+      <section
+        id="home"
+        className="section home-section d-flex align-items-center"
+      >
+        <Container className="d-flex justify-content-center">
+          <Card className="home-card text-center p-4">
+            <Card.Body>
+              <img src={logo} alt="Logo" className="home-logo mb-4" />
+              <Card.Title className="mb-3">
+                JOSIMAR BEZERRA & ADVOGADOS
+              </Card.Title>
+              <Card.Subtitle className="mb-3 fs-4">
+                Direito condominial - Expulsão de Condôminos antissociais
+              </Card.Subtitle>
+              <Card.Text className="mb-3">
+                O Escritório de advocacia JOSIMAR BEZERRA & ADVOGADOS ASSOCIADOS
+                atua com foco em direito condominial e gestão de conflitos com
+                condôminos antissociais.
+              </Card.Text>
+              <Card.Text>
+                Contamos com uma equipe altamente qualificada, com ampla
+                experiência em soluções jurídicas voltadas ao setor condominial.
+                Nosso compromisso é auxiliar nossos clientes na busca pela tão
+                desejada harmonia e tranquilidade nas relações comunitárias,
+                oferecendo um atendimento personalizado e ágil, com ética e
+                excelência.
+              </Card.Text>
+              <Button variant="success" className="whatsapp-button mt-4">
+                FALE COM ESPECIALISTA
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
+      </section>
+
+      <section
+        id="about"
+        className="section about-section d-flex align-items-center"
+      >
+        <Container className="d-flex justify-content-center">
+          <Card className="p-4 about-card">
+            <Card.Body>
+              <Card.Title>O que é Direito condominial?</Card.Title>
+              <Card.Text>
+                O Direito Condominial é a área jurídica que regulamenta as
+                relações e resolve conflitos no âmbito dos condomínios. Ele
+                estabelece regras para o uso adequado das propriedades e prevê
+                penalidades em casos de descumprimento. Essas normas estão
+                previstas no Código Civil e na legislação condominial
+                específica.
+              </Card.Text>
+              <Card.Text>
+                Por isso, é obrigação dos condôminos cumprir tanto as
+                disposições do Código Civil quanto as normas internas que regem
+                a convivência em condomínios. O Direito Condominial define
+                direitos e deveres, assegurando a ordem e a harmonia na
+                coletividade condominial.
+              </Card.Text>
+              <Card.Text>
+                No nosso escritório, os advogados especializados em Direito
+                Condominial estão capacitados para resolver conflitos com base
+                na legislação vigente. Além disso, orientam e promovem ações
+                específicas, como a expulsão de condôminos antissociais,
+                garantindo que os direitos dos demais sejam preservados.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      </section>
+
+      <section
+        id="contact"
+        className="section contact-section d-flex align-items-center"
+      >
+        <Container className="d-flex justify-content-center">
+          <Card className="text-center p-4 contact-card">
+            <Card.Body>
+              <Card.Title>Contato</Card.Title>
+              <Card.Text>Entre em contato conosco!</Card.Text>
+              <Card.Text>Email: contato@exemplo.com</Card.Text>
+              <Card.Text>Telefone: (31) 1234-5678</Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      </section>
+
+      <footer className="bg-dark text-light py-4">
+        <Container>
+          <p className="text-center">
+            &copy; 2024 Minha Empresa. Todos os direitos reservados.
+          </p>
+        </Container>
+      </footer>
     </div>
   );
 }
--- src/components/Menubar/Menubar.jsx
+++ src/components/Menubar/Menubar.jsx
@@ -2,37 +2,60 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-scroll";
+import "./Menubar.scss";
 
 const Menubar = () => {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar sticky="top" expand="lg" className="navbar-fixed bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Item>
+              <Link
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="nav-link"
+              >
+                Home
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="nav-link"
+              >
+                Sobre
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className="nav-link"
+              >
+                Contato
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
